9324d346389fcce5f7f43555ca8eacd86ce33939
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Getting Started - Lesson 3 - Using of the Series Template for Auto-Created Series
+
+
+<p>This lesson demonstrates how to add a series template to a chart's diagram, and then automatically bind this template to a datasource.</p><p>Note that although in this tutorial we will bind the chart to a<strong> </strong><strong>GSP.xm</strong><strong>l</strong> datasource, DXCharts supports a wide range of data providers. For more information on data binding, refer to the <a href="http://help.devexpress.com/#Silverlight/CustomDocument5364"><u>Providing Data</u></a>  section.</p><p></p>
+
+<br/>
+
+
---
+++ VB/SilverlightApplication3/MainPage.xaml.vb
@@ -0,0 +1,70 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Controls
+Imports System.Globalization
+Imports System.Xml.Linq
+
+Namespace SilverlightApplication3
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			chartControl1.DataSource = CreateDataSource()
+		End Sub
+
+		Private Function CreateDataSource() As List(Of GSP)
+			Dim document As XDocument = DataLoader.LoadXmlFromResources("/GSP.xml")
+			Dim result As New List(Of GSP)()
+			If document IsNot Nothing Then
+				For Each element As XElement In document.Element("GSPs").Elements()
+					Dim region As String = element.Element("Region").Value
+					Dim year As String = element.Element("Year").Value
+					Dim product As Double = Convert.ToDouble(element.Element("Product").Value, CultureInfo.InvariantCulture)
+					result.Add(New GSP(region, year, product))
+				Next element
+			End If
+			Return result
+		End Function
+
+		Public NotInheritable Class DataLoader
+			Private Sub New()
+			End Sub
+			Public Shared Function LoadXmlFromResources(ByVal fileName As String) As XDocument
+				Try
+					Return XDocument.Load("/SilverlightApplication3;component" & fileName)
+				Catch
+					Return Nothing
+				End Try
+			End Function
+		End Class
+
+		Public Class GSP
+			Private ReadOnly region_Renamed As String
+			Private ReadOnly year_Renamed As String
+			Private ReadOnly product_Renamed As Double
+
+			Public ReadOnly Property Region() As String
+				Get
+					Return region_Renamed
+				End Get
+			End Property
+			Public ReadOnly Property Year() As String
+				Get
+					Return year_Renamed
+				End Get
+			End Property
+			Public ReadOnly Property Product() As Double
+				Get
+					Return product_Renamed
+				End Get
+			End Property
+
+			Public Sub New(ByVal region As String, ByVal year As String, ByVal product As Double)
+				Me.region_Renamed = region
+				Me.year_Renamed = year
+				Me.product_Renamed = product
+			End Sub
+		End Class
+	End Class
+End Namespace
